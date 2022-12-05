40374c40a0037d02ddb86f9b886b47af0bd51307
Naseerkhanp <naseerkhan200@gmail.com>
2022-12-05 23:02:20 +0530

    removed
    done changes

--- InboundProperties.java
+++ InboundProperties.java
@@ -1,4 +1,4 @@
-package com.excelacom.activation.properties;
+package com.activation.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
